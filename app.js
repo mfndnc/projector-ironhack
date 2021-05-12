0d23b24fc8cb62ff912447f4d668667bb050fec6
Marcia Faria <mfndnc@gmail.com>
2021-05-12 10:42:59 +0200

    working locally

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require("./config")(app);
-
+require('./config')(app);
 
 // session configuration
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const DB_URL = 'mongodb://localhost/projector';
-
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/projector';
 
 app.use(
   session({
@@ -29,13 +27,12 @@ app.use(
     saveUninitialized: false,
     resave: true,
     store: MongoStore.create({
-      mongoUrl: DB_URL
-    })
+      mongoUrl: MONGO_URI,
+    }),
   })
-)
+);
 
 // end of session configuration
-
 
 // passport configuration
 // http://www.passportjs.org/docs/configure/
@@ -44,30 +41,30 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
-// passport wants to store as little data as possible in the session so it only uses 
-// the id's (or someting else if we would want to implement that) and not the whole 
+// passport wants to store as little data as possible in the session so it only uses
+// the id's (or someting else if we would want to implement that) and not the whole
 // user object
 // this method is used by passport to put the id of the user into the session
 passport.serializeUser((user, done) => {
   done(null, user._id);
-})
+});
 
 // this is used to retrieve the user by it's id (that is stored in the session)
 passport.deserializeUser((id, done) => {
   User.findById(id)
-    .then(dbUser => {
-      done(null, dbUser)
+    .then((dbUser) => {
+      done(null, dbUser);
     })
-    .catch(err => {
+    .catch((err) => {
       done(err);
-    })
-})
+    });
+});
 
 passport.use(
   new LocalStrategy((username, password, done) => {
     // this logic will be executed when we log in
     User.findOne({ username: username })
-      .then(userFromDB => {
+      .then((userFromDB) => {
         if (userFromDB === null) {
           // there is no user with this username
           done(null, false, { message: 'Wrong Credentials' });
@@ -79,31 +76,27 @@ passport.use(
           done(null, userFromDB);
         }
       })
-      .catch(err => {
+      .catch((err) => {
         next(err);
-      })
+      });
   })
-)
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // end of passport configuration
 
-
-
-
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controled from the routes/index.js
 
-const projects = require("./routes/projects");
-app.use("/api/projects", projects);
+const projects = require('./routes/projects');
+app.use('/api/projects', projects);
 
-const auth = require("./routes/auth");
-app.use("/api/auth", auth);
-
+const auth = require('./routes/auth');
+app.use('/api/auth', auth);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
