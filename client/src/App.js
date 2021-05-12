f06adf0aa3a59f0b18663c1f95524b4147f1ad15
Marcia Faria <mfndnc@gmail.com>
2021-05-12 13:23:15 +0200

    testing features in heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Projects from './components/Projects';
 import React from 'react';
 import Signup from './components/Signup';
@@ -9,18 +9,17 @@ import ProtectedRoute from './components/ProtectedRoute';
 import Navbar from './components/Navbar';
 
 class App extends React.Component {
-
   state = {
-    user: this.props.user
-  }
+    user: this.props.user,
+  };
 
-  setUser = user => {
+  setUser = (user) => {
     this.setState({ user });
-  }
+  };
 
   render() {
     return (
-      <div className="App" >
+      <div className="App">
         <Navbar user={this.state.user} setUser={this.setUser} />
         {/* <Route exact path="/projects" component={Projects} /> */}
         {/* The route to '/projects' should now be protected -> if there is no logged in user
@@ -37,21 +36,22 @@ class App extends React.Component {
         /> */}
 
         <ProtectedRoute
-          path='/projects'
+          path="/projects"
           user={this.state.user}
           component={Projects}
-          redirectPath='/login'
+          redirectPath="/login"
         />
 
         <Route exact path="/projects/:id" component={ProjectDetails} />
 
         <Route
-          exact path="/signup"
+          exact
+          path="/signup"
           // component={Signup}
           // we need to use the render prop to be able to pass props to the Signup component
           // and for this we need to be able to reference the component like this: <Signup />
           // https://reactjs.org/docs/render-props.html
-          render={props => <Signup setUser={this.setUser} {...props} />}
+          render={(props) => <Signup setUser={this.setUser} {...props} />}
         />
         {/* if you don't want to use the render prop but still need to pass props to the 
          component referenced in the route*/}
@@ -59,8 +59,9 @@ class App extends React.Component {
           <Login setUser={this.setUser} {...this.props} />
         </Route> */}
         <Route
-          exact path="/login"
-          render={props => <Login setUser={this.setUser} {...props} />}
+          exact
+          path="/login"
+          render={(props) => <Login setUser={this.setUser} {...props} />}
         />
       </div>
     );
